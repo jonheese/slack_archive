573cd3ce036f9cc3da943e8d4998724c90829e75
Jon Heese <git@jonheese.com>
2020-11-19 09:41:17 -0500

    Various fixes/improvements:
    - Port to py3
    - Add favicon and header image
    - Handle missing emojis better
    - Add minus character as "NOT" logic
    - Rearrange URL params, convert to POST for context stuff
    - Make results table striped (again)
    - Enable dark mode

--- slack_archive.py
+++ slack_archive.py
@@ -6,9 +6,10 @@ import math
 import mysql.connector
 import re
 import sys
+import urllib.parse
 from config import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
 from datetime import datetime
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, send_file
 from mysql.connector import Error
 from random import randint
 
@@ -22,11 +23,39 @@ reserved_words = [
     "href",
 ]
 
+
+def set_url_param(
+    urlpath,
+    param_name,
+    param_value,
+):
+    param_dict = {}
+    param_value = str(param_value)
+    if "?" in urlpath:
+        [url, query] = urlpath.split("?")
+    else:
+        url = urlpath
+        query = ""
+    for pair in query.split("&"):
+        key = value = None
+        if "=" in pair:
+            [key, value] = pair.split("=")
+        if key and value:
+            param_dict[key] = value
+    param_dict[param_name] = urllib.parse.quote(param_value).replace("%20", "+")
+    url = url + "?"
+    for key, value in param_dict.items():
+        url = url + f"{key}={value}&"
+    return url[:-1]
+
+
 def get_db_conn():
-    conn = mysql.connector.connect(host=DB_HOST,
-                                   database=DB_NAME,
-                                   user=DB_USER,
-                                   password=DB_PASSWORD)
+    conn = mysql.connector.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+    )
     return conn
 
 
@@ -72,7 +101,7 @@ def get_files(message_id):
 
 def format_pre(text, delimiter, tagclass=None):
     if tagclass:
-        tag = "pre class=%s" % tagclass
+        tag = f"pre class={tagclass}"
     else:
         tag = "pre"
     if text.count(delimiter) > 1 and text.count(delimiter) % 2 == 0:
@@ -80,7 +109,7 @@ def format_pre(text, delimiter, tagclass=None):
         for i, block in enumerate(split_block):
             if i % 2 == 0:
                 continue
-            text = text.replace('%s%s%s' % (delimiter, block, delimiter), '<%s>%s</%s>' % (tag, block.strip(), tag.split()[0]))
+            text = text.replace(f'{delimiter}{block}{delimiter}', f'<{tag}>{block.strip()}</{tag.split()[0]}>')
     return text
 
 
@@ -98,19 +127,20 @@ def format_line_blockquote(text):
         else:
             if in_quote:
                 in_quote = False
-                out_text += "<blockquote>%s</blockquote>%s" % (quote_block, line)
+                out_text += f"<blockquote>{quote_block}</blockquote>{line}"
                 quote_block = ""
             else:
                 out_text += line
     if in_quote:
-        out_text += "<blockquote>%s</blockquote>" % quote_block
+        out_text += f"<blockquote>{quote_block}</blockquote>"
     return out_text
 
 
 def format_full_blockquote(text):
     split_text = text.split("&gt;&gt;&gt;")
     header = split_text[0]
-    quote = "<blockquote>%s</blockquote>" % '\n'.join(split_text[1:])
+    lines = '\n'.join(split_text[1:])
+    quote = f"<blockquote>{lines}</blockquote>"
     return header + quote
 
 
@@ -123,11 +153,15 @@ def format_user_mentions_and_emojis(text):
             end_index = word.index(">")
             if end_index > start_index:
                 slack_user_id = word[start_index + 2:end_index - start_index].split("|")[0]
-                if slack_user_id in username_by_slack_user_id.keys():
+                if slack_user_id in username_by_slack_user_id.keys() and username_by_slack_user_id[slack_user_id]:
                     text = text.replace(word, "@" + username_by_slack_user_id[slack_user_id])
+                else:
+                    text = text.replace(word, "@" + slack_user_id)
         if word.startswith(":") and word.endswith(":"):
             emoji_trigger = word[1:-1]
-            text = text.replace(word, emoji.emojize(emoji_trigger, use_aliases=True))
+            emoji_text = emoji.emojize(emoji_trigger, use_aliases=True)
+            if emoji_text != emoji_trigger:
+                text = text.replace(word, emoji_text)
     return text
 
 
@@ -138,14 +172,14 @@ def format_links(text):
             end_index = word.index(">")
             if end_index > start_index:
                 url = word[start_index + 1:end_index - start_index].split("|")[0]
-                text = text.replace(word, "<a href='%s'>%s</a>" % (url, url))
+                text = text.replace(word, f"<a href='{url}'>{url}</a>")
     return text
 
 
 def format_simple_tag(text, delimiter, tag):
     for word in text.split():
         if word.startswith(delimiter) and word.endswith(delimiter):
-            text = text.replace(word, '<%s>%s</%s>' % (tag, word[1:-1], tag))
+            text = text.replace(word, f'<{tag}>{word[1:-1]}</{tag}>')
     return text
 
 
@@ -165,9 +199,9 @@ def package_messages(messages, q):
         text = format_simple_tag(text, '*', 'b')
         text = text.replace('\n', '<br />')
         if q and q not in reserved_words:
-            #q_pattern = re.compile(" %s" % q, re.IGNORECASE)
-            #text = q_pattern.sub(" <b>%s</b>" % q, text)
-            text = text.replace(q, "<b>%s</b>" % q)
+            #q_pattern = re.compile(f" {q}", re.IGNORECASE)
+            #text = q_pattern.sub(f" <b>{q}</b>", text)
+            text = text.replace(q, f"<b>{q}</b>")
         record = {
             "team_id": message[0],
             "channel_id": message[1],
@@ -182,11 +216,11 @@ def package_messages(messages, q):
             "files": get_files(message[6]),
         }
         if not record["channel_name"]:
-            record["channel_name"] = "Unknown (%s)" % message[8]
+            record["channel_name"] = f"Unknown ({message[8]})"
         if not record["username"]:
-            record["username"] = "Unknown (%s)" % message[9]
+            record["username"] = f"Unknown ({message[9]})"
         if not record['full_name']:
-            record['full_name'] = "Unknown (%s)" % record['username']
+            record['full_name'] = f"Unknown ({record['username']})"
         if message[11]:
             record['avatar_url'] = message[11]
         else:
@@ -196,12 +230,18 @@ def package_messages(messages, q):
 
 
 def query_context_messages(channel_id, timestamp, direction, comparison, limit, offset):
-    query = "select m.team_id, m.channel_id, t.team_name, u.username, m.timestamp, m.text, m.id, c.channel_name, c.slack_channel_id, u.slack_user_id, u.full_name, u.avatar_url from " + \
+    query = "select m.team_id, m.channel_id, t.team_name, u.username, m.timestamp, m.text, m.id, " + \
+            "c.channel_name, c.slack_channel_id, u.slack_user_id, u.full_name, u.avatar_url from " + \
             "tbl_messages m join tbl_users u on u.id = m.user_id join tbl_channels c on c.id = m.channel_id " + \
-            "join tbl_teams t on t.id = m.team_id where m.channel_id = %s and timestamp " + comparison + " %s order by timestamp " + direction + " limit " + \
-            str(abs(offset)) + ", " + str(limit)
+            "join tbl_teams t on t.id = m.team_id where m.channel_id = %s and timestamp " + comparison + \
+            " %s order by timestamp " + direction + " limit " + str(abs(offset)) + ", " + str(limit)
     print(query)
     return do_select(query, (channel_id, timestamp))
+
+
+@app.route('/favicon.ico', methods=['GET'])
+def get_favicon():
+    return send_file('static/favicon.ico')
 
 
 @app.route('/', methods=['GET'])
@@ -222,17 +262,26 @@ def search():
         where_columns = []
         where_values = []
         if username:
-            where_columns.append(" (u.username LIKE '%%%s%%' OR u.slack_user_id LIKE '%%%s%%' or u.full_name like '%%%s%%') ")
-            where_values.append(username)
-            where_values.append(username)
-            where_values.append(username)
+            if username.startswith("-"):
+                where_columns.append(" (u.username NOT LIKE '%%%s%%' AND u.slack_user_id NOT LIKE '%%%s%%' AND u.full_name NOT like '%%%s%%') ")
+                where_values.extend([username[1:], username[1:], username[1:]])
+            else:
+                where_columns.append(" (u.username LIKE '%%%s%%' OR u.slack_user_id LIKE '%%%s%%' or u.full_name like '%%%s%%') ")
+                where_values.extend([username, username, username])
         if team_name:
-            where_columns.append(" t.team_name LIKE '%%%s%%' ")
-            where_values.append(team_name)
+            if team_name.startswith("-"):
+                where_columns.append(" t.team_name NOT LIKE '%%%s%%' ")
+                where_values.extend(team_name[1:])
+            else:
+                where_columns.append(" t.team_name LIKE '%%%s%%' ")
+                where_values.append(team_name)
         if channel_name:
-            where_columns.append(" (c.channel_name LIKE '%%%s%%' OR c.slack_channel_id LIKE '%%%s%%') ")
-            where_values.append(channel_name)
-            where_values.append(channel_name)
+            if channel_name.startswith("-"):
+                where_columns.append(" (c.channel_name NOT LIKE '%%%s%%' AND c.slack_channel_id NOT LIKE '%%%s%%') ")
+                where_values.extend([channel_name[1:], channel_name[1:]])
+            else:
+                where_columns.append(" (c.channel_name LIKE '%%%s%%' OR c.slack_channel_id LIKE '%%%s%%') ")
+                where_values.extend([channel_name, channel_name])
         if start_date:
             where_columns.append(" m.timestamp > %s ")
             where_values.append(datetime.strptime(start_date, "%Y-%m-%d").strftime("%s"))
@@ -248,12 +297,13 @@ def search():
         if not where_columns:
             return render_template("results.j2", messages=[])
         where_clause = " WHERE " + " AND ".join(where_columns)
-        limit_clause = " LIMIT %s, %s" % (offset, results_per_page)
+        limit_clause = f" LIMIT {offset}, {results_per_page}"
         count_query = "select count(m.id) from " + \
                 "tbl_messages m join tbl_users u on u.id = m.user_id join tbl_channels c on c.id = m.channel_id " + \
                 "join tbl_teams t on t.id = m.team_id " + where_clause
         total_count = int(do_select(count_query, tuple(where_values))[0][0])
-        query = "select m.team_id, m.channel_id, t.team_name, u.username, m.timestamp, m.text, m.id, c.channel_name, c.slack_channel_id, u.slack_user_id, u.full_name, u.avatar_url from " + \
+        query = "select m.team_id, m.channel_id, t.team_name, u.username, m.timestamp, m.text, m.id, c.channel_name, " + \
+                "c.slack_channel_id, u.slack_user_id, u.full_name, u.avatar_url from " + \
                 "tbl_messages m join tbl_users u on u.id = m.user_id join tbl_channels c on c.id = m.channel_id " + \
                 "join tbl_teams t on t.id = m.team_id " + where_clause + " order by timestamp " + limit_clause
         messages = package_messages(do_select(query, tuple(where_values)), q)
@@ -267,14 +317,15 @@ def search():
             range_start = 0
         else:
             range_start = offset + 1
-        message_range = "%s - %s" % (range_start, min(offset+results_per_page, total_count))
+        message_range = f"{range_start} - {min(offset+results_per_page, total_count)}"
         param_string = []
         for param_name in request.args.keys():
             if param_name != "page":
-                param_string.append("%s=%s" % (param_name, request.args[param_name]))
+                param_value = request.args[param_name].replace(' ', '+')
+                param_string.append(f"{param_name}={param_value}")
         param_string = "&".join(param_string)
-        prev_page_url = "/?%s&page=%s" % (param_string, page-1)
-        next_page_url = "/?%s&page=%s" % (param_string, page+1)
+        prev_page_url = f"/?{param_string}&page={page-1}"
+        next_page_url = f"/?{param_string}&page={page+1}"
         return render_template("results.j2",
                                q=q,
                                team_name=team_name,
@@ -290,24 +341,39 @@ def search():
                                page=page,
                                message_range=message_range,
                                total_count=total_count,
+                               param_string=param_string,
                                messages=messages)
     except Error as e:
-        return render_template("results.j2", q="Exception encountered: %s" % e)
+        return render_template("results.j2", q=f"Exception encountered: {e}")
 
 
-@app.route('/context/<message_id>/', methods=['GET'])
-@app.route('/context/<message_id>/<q>', methods=['GET'])
-@app.route('/context/<message_id>/<q>/', methods=['GET'])
-@app.route('/context/<message_id>/<q>/<offset>', methods=['GET'])
-def context(message_id, q="", offset=0):
+@app.route('/context/<message_id>/', methods=['GET', 'POST'])
+@app.route('/context/<message_id>/<q>', methods=['GET', 'POST'])
+@app.route('/context/<message_id>/<q>/', methods=['GET', 'POST'])
+def context(message_id, q=None):
     try:
-        query = "select m.team_id, m.channel_id, m.timestamp, m.text, u.full_name from tbl_messages m join tbl_users u on u.id = m.user_id where m.id = %s"
+        if not q:
+            q = request.form.get("q", request.args.get("q", ""))
+        offset = request.form.get("offset", request.args.get("offset", 0))
+        username = request.form.get("username", request.args.get("username", ""))
+        channel_name = request.form.get("channel_name", request.args.get("channel_name", ""))
+        team_name = request.form.get("team_name", request.args.get("team_name", ""))
+        start_date = request.form.get("start_date", request.args.get("start_date", ""))
+        end_date = request.form.get("end_date", request.args.get("end_date", ""))
+        sql_match = request.form.get("sql_match", request.args.get("sql_match", False))
+        if sql_match == "True":
+            sql_match = True
+        else:
+            sql_match = False
+
+        query = "select m.team_id, m.channel_id, m.timestamp, m.text, u.full_name from " + \
+                "tbl_messages m join tbl_users u on u.id = m.user_id where m.id = %s"
         messages = do_select(query, message_id)
         team_id = messages[0][0]
         channel_id = messages[0][1]
         timestamp = messages[0][2]
         datetime_string = datetime.fromtimestamp(timestamp).strftime('%I:%M %p %Y-%m-%d (%a)')
-        summary = "%s (%s): %s" % (messages[0][4], datetime_string, messages[0][3])
+        summary = f"{messages[0][4]} ({datetime_string}): {messages[0][3]}"
         window_size = 20
         try:
             offset = int(offset)
@@ -325,11 +391,11 @@ def context(message_id, q="", offset=0):
         if after_limit > 0:
             after_messages = package_messages(query_context_messages(channel_id, timestamp, "asc", ">", after_limit, sql_offset), q)
             messages.extend(after_messages)
-        username = ""
+        this_username = ""
         color = "#ffffff"
         date = None
         for message in messages:
-            if message["username"] != username:
+            if message["username"] != this_username:
                 if color == "#ffffff":
                     message["color"] = "#cccccc"
                 else:
@@ -341,7 +407,46 @@ def context(message_id, q="", offset=0):
             message["display_date"] = message["date"] != date
             date = message["date"]
             color = message["color"]
-            username = message["username"]
-        return render_template("context.j2", messages=messages, q=q, offset=offset, message_id=message_id, summary=summary, urlpath=request.path)
+            this_username = message["username"]
+        urlpath = request.path
+
+        prev20_url = set_url_param(urlpath, "offset", offset - 20)
+        prev_url = set_url_param(urlpath, "offset", offset - 1)
+        next_url = set_url_param(urlpath, "offset", offset + 1)
+        next20_url = set_url_param(urlpath, "offset", offset + 20)
+
+        return render_template(
+            "context.j2",
+            messages=messages,
+            q=q,
+            offset=offset,
+            message_id=message_id,
+            summary=summary,
+            urlpath=urlpath,
+            next_url=next_url,
+            prev_url=prev_url,
+            next20_url=next20_url,
+            prev20_url=prev20_url,
+            username=username,
+            team_name=team_name,
+            channel_name=channel_name,
+            sql_match=sql_match,
+            start_date=start_date,
+            end_date=end_date,
+        )
     except Error as e:
-        return render_template("context.j2", errors=["Database query error: %s" % e], messages=None, q=q, offset=offset, message_id=message_id)
+        return render_template(
+            "context.j2",
+            errors=[f"Database query error: {e}"],
+            messages=None,
+            urlpath=urlpath,
+            q=q,
+            offset=offset,
+            message_id=message_id,
+            username=username,
+            channel_name=channel_name,
+            team_name=team_name,
+            sql_match=sql_match,
+            start_date=start_date,
+            end_date=end_date,
+        )
